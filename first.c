8dd77c7067ceaba792f7a9077ef4bcbefce4d627
saurav <saurav878901@gmail.com>
2025-05-24 12:53:34 +0530

    First commit

---
+++ first.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+ int a;   
+printf("enter your no");
+scanf("%d",&a);
+
+ int b;   
+printf("enter your no");
+scanf("%d",&b);
+
+int c=a+b;
+printf("sum%d ",c);
+
+return 0;
+}
